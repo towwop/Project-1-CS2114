3bd6b728c0f8d4aba8d3c24eef92da1febe80bd6
Abdullah Zureikat <zureikatabdullah@gmail.com>
2026-09-24 20:47:17 -0400

    style(Main): formatted code with C-S-f keybind

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,23 +6,21 @@ import java.util.Scanner;
  * @author Oscar, Abdullah, Vihaan
  * @version 2026.09.24
  */
-public class Main
-{
+public class Main {
     /**
      * Runs the task manager until the user quits.
      *
      * @param args
      *            command-line arguments (unused)
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         TaskStorage storage = new TaskStorage();
         TUI ui = new TUI(storage, scanner);
-        
+
         boolean running = true;
-        
+
         while (running) {
             ui.printTaskList();
             running = ui.handleUserAction();
